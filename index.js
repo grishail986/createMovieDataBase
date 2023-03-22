7e3e7b5972b1e8c4b2db0c57b476d7e242ff021b
Gregory Ilchenko <grishail986@gmail.com>
2023-03-22 12:00:07 +0400

    init: start task

---
+++ index.js
@@ -0,0 +1,19 @@
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '')
+
+personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
+
+let a = prompt('Один из последних просмотренных фильмов?', '')
+let b = prompt('На сколько оцените его?', '')
+let c = prompt('Один из последних просмотренных фильмов?', '')
+let d = prompt('На сколько оцентите его?', '')
+
+personalMovieDB.movies[a] = b
+personalMovieDB.movies[c] = d
+
+console.log(personalMovieDB)
