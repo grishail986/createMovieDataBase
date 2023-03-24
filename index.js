ef37022dfaa5ad2da0a23bdedd9ab863fa2d6ea8
Gregory Ilchenko <grishail986@gmail.com>
2023-03-24 21:19:00 +0400

    feat: update README and automated questions to the user

--- index.js
+++ index.js
@@ -8,12 +8,20 @@ personalMovieDB = {
     privat: false
 }
 
-let a = prompt('Один из последних просмотренных фильмов?', '')
-let b = prompt('На сколько оцените его?', '')
-let c = prompt('Один из последних просмотренных фильмов?', '')
-let d = prompt('На сколько оцентите его?', '')
+// let a = prompt('Один из последних просмотренных фильмов?', '')
+// let b = prompt('На сколько оцените его?', '')
+// let c = prompt('Один из последних просмотренных фильмов?', '')
+// let d = prompt('На сколько оцентите его?', '')
+let a
+let b
+
+for (i = 0; i < 2; i++) {
+    a = prompt('Один из последних просмотренных фильмов?', '')
+    b = prompt('На сколько оцените его?', '')
+    personalMovieDB.movies[a] = b
+}
 
 personalMovieDB.movies[a] = b
-personalMovieDB.movies[c] = d
+// personalMovieDB.movies[c] = d
 
 console.log(personalMovieDB)
