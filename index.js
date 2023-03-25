477c34ab09674b5fe93c1a46c5d8e83f6f0e9540
Gregory Ilchenko <grishail986@gmail.com>
2023-03-25 11:18:42 +0400

    feat: add user interaction and check movieDB

--- index.js
+++ index.js
@@ -12,16 +12,43 @@ personalMovieDB = {
 // let b = prompt('На сколько оцените его?', '')
 // let c = prompt('Один из последних просмотренных фильмов?', '')
 // let d = prompt('На сколько оцентите его?', '')
+
 let a
 let b
 
 for (i = 0; i < 2; i++) {
     a = prompt('Один из последних просмотренных фильмов?', '')
+    if (a == '' || a == null || a.length > 50) {
+        prompt('Один из последних просмотренных фильмов?', '')
+    }
     b = prompt('На сколько оцените его?', '')
+    if (b == '' || b == null || b.length > 50) {
+        prompt('На сколько оцените его?', '')
+    }
     personalMovieDB.movies[a] = b
 }
 
-personalMovieDB.movies[a] = b
+if (personalMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов')
+} else if (personalMovieDB.count <= 30) {
+    alert('Вы классический зритель')
+} else if (personalMovieDB.count > 30) {
+    alert('Вы киноман')
+} else {
+    alert('Произошла ошибка')
+
+}
+
+// i = 0 
+
+// while (i < 2) {
+//     a = prompt('Один из последних просмотренных фильмов?', '')
+//     b = prompt('На сколько оцените его?', '')
+//     personalMovieDB.movies[a] = b
+//     i = i + 1
+// }
+
+// personalMovieDB.movies[a] = b
 // personalMovieDB.movies[c] = d
 
 console.log(personalMovieDB)
